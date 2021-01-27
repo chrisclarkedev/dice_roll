68aa0514f22589cc337550cb1517df3181c7d18f
chrisclarkedev <cclarke8904@gmail.com>
2021-01-27 18:49:47 -0500

    change location of image files, added dice rolling functionality

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const score1El = document.getElementById("score--1");
 // Element for the dice
 const diceEl = document.querySelector(".dice");
 
+// Element for the different buttons
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -16,3 +21,15 @@ score1El.textContent = 0;
 // Hides the dice element
 diceEl.classList.add("hidden");
 // Select scores for player 1 and 2 and change them both to 0
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice roll (1-6)
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display the dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1: if true, switch to next player
+});
