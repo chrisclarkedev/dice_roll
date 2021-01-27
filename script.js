0d23c9e16919a50570df4564a5e742bd2c6d3523
chrisclarkedev <cclarke8904@gmail.com>
2021-01-27 18:03:22 -0500

    query selector for score element

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// Both of these do the same thing buy getElementById is a bit faster and is usually used when
+// trying to do multiple items with different id's
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
