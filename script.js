30f89817b3d15b90ed8594069dadbb0688ef2962
chrisclarkedev <cclarke8904@gmail.com>
2021-01-31 18:44:46 -0500

    switching of players when 1 is rolled

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 // Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
 // Element for the dice
 const diceEl = document.querySelector(".dice");
@@ -18,6 +20,12 @@ const btnHold = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 
+// Current Score always set to 0 as beginning of game
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 // Hides the dice element
 diceEl.classList.add("hidden");
 // Select scores for player 1 and 2 and change them both to 0
@@ -30,6 +38,19 @@ btnRoll.addEventListener("click", function () {
 
   // 2. Display the dice
   diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
+  diceEl.src = `dice_${dice}.png`;
+
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    // switch to next player
+    // If active player is 0 then the new active player should be 1
+    // else it should be 0
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
 });
